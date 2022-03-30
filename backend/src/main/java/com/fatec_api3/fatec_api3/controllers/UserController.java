8736aee2baf249121ca141a9df49959ddc1dd52b
GabrielCamargoL <gabriel.cleite@outlook.com>
2022-03-30 17:03:45 -0300

    encrypt user password

--- backend/src/main/java/com/fatec_api3/fatec_api3/controllers/UserController.java
+++ backend/src/main/java/com/fatec_api3/fatec_api3/controllers/UserController.java
@@ -7,6 +7,10 @@ import com.fatec_api3.fatec_api3.entities.User;
 import com.fatec_api3.fatec_api3.repositories.UserRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+// import org.springframework.data.mongodb.core.query.Criteria;
+// import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,9 +20,11 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserController {
-  
+
   @Autowired
   UserRepository repositoryUser;
+
+  MongoTemplate mongoTemplate;
 
   @GetMapping("/users")
   public List<User> getAllUsers() {
@@ -31,7 +37,7 @@ public class UserController {
   public User getOneUser(@PathVariable String userId) {
     Optional<User> userOptional = repositoryUser.findById(userId);
 
-    if (userOptional.isEmpty()){
+    if (userOptional.isEmpty()) {
 
       return null;
     } else {
@@ -47,6 +53,10 @@ public class UserController {
     User userExists = repositoryUser.findByEmail(reqUser.getEmail());
     
     if (userExists == null) {
+      BCryptPasswordEncoder criptografar = new BCryptPasswordEncoder();
+      String senhaCriptografada = criptografar.encode(reqUser.getPassword());
+      reqUser.setPassword(senhaCriptografada);
+
       User user = repositoryUser.insert(reqUser);
       return user;
 
@@ -55,9 +65,30 @@ public class UserController {
     }
   }
 
+  // @PostMapping("/user")
+  // public User createUser(@RequestBody User reqUser) {
+
+  //   Query query = new Query();
+  //   query.addCriteria(Criteria.where("email").is(reqUser.getEmail()));
+
+  //   try {
+  //     User usersExists = repositoryUser
+  //         .findUserByEmail(reqUser.getEmail())
+  //         .ifPresentOrElse(u -> {
+  //           System.out.println(u + " já existe");
+
+  //         }, () -> {
+  //           repositoryUser.insert(reqUser);
+  //         });
+
+  //   } catch (Exception e) {
+  //     System.out.println(e);
+  //   }
+  // }
+
   @PutMapping("/user/")
   public User updateUser(@RequestBody User reqUser) {
-    
+
     Optional<User> userOptional = repositoryUser.findById(reqUser.getId());
     if (userOptional.isEmpty()) {
       return null;
--- backend/src/main/java/com/fatec_api3/fatec_api3/repositories/UserRepository.java
+++ backend/src/main/java/com/fatec_api3/fatec_api3/repositories/UserRepository.java
@@ -1,11 +1,14 @@
 package com.fatec_api3.fatec_api3.repositories;
 
-import java.util.List;
+
+import java.util.Optional;
 
 import com.fatec_api3.fatec_api3.entities.User;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface UserRepository extends MongoRepository<User, String> {
+  Optional<User> findUserByEmail(String email);
+
   User findByEmail(String email);
 }
